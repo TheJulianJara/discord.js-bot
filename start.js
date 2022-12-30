a1b5b017984b41839b866119234aa596909016df
Julián Jara <56706662+TheJulianJara@users.noreply.github.com>
2022-12-30 02:18:54 -0300

    Create start.js

---
+++ start.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const config = require('./config.js');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').forEach(dir => {
+    const commandFiles = fs.readdirSync(`./commands/${dir}/`).filter(f => f.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${dir}/${file}`);
+        commands.push(command.data.toJSON());
+    }
+})
+
+const rest = new REST({ version: '9' }).setToken(config.bot.token);
+
+(async() => {
+    try {
+        console.log('Se inició el registro de slash commands.');
+
+        await rest.put(
+            Routes.applicationCommands(config.bot.clientId, config.bot.guildId), { body: commands },
+        );
+
+        console.log('Se registraron slash commands correctamente. Por favor, inicie el bot nuevamente.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
