353bafa73e57d7092a8b969c9515b5028e32e462
Julián Jara <56706662+TheJulianJara@users.noreply.github.com>
2024-01-08 03:57:30 -0300

    Update config.js with empty values

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 let bot = {
-    token: "MTE5MzgwNzk2ODk5MTQ2MTQzNg.GaZnHn.VeNXhsQS7D6E1Et5EGJDiPqGs2zZOmEY07t0Ag",
-    id: '1193807968991461436',
+    token: "",
+    id: '',
     dev: {
-        id: '884273853956755487',
+        id: '',
     }
 }
 let db = {
-    url: "mongodb+srv://nicoblanco208:dRnI9Oplvz5chlm0@cluster0.7n1uhyu.mongodb.net/?retryWrites=true&w=majority",
+    url: "",
 }
 
 let guild = {
-    id: '1105241952565346444',
+    id: '',
 }
 
 module.exports = { bot, db, guild }
