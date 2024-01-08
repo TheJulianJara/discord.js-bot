38a2b74d1bc4ef85a5220eb4bbfa779a10f54ceb
Julián Jara <56706662+TheJulianJara@users.noreply.github.com>
2024-01-08 03:56:55 -0300

    Update config.js with token, id, and URL

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 let bot = {
-    token: "",
-    id: '',
+    token: "MTE5MzgwNzk2ODk5MTQ2MTQzNg.GaZnHn.VeNXhsQS7D6E1Et5EGJDiPqGs2zZOmEY07t0Ag",
+    id: '1193807968991461436',
     dev: {
-        id: '',
+        id: '884273853956755487',
     }
 }
 let db = {
-    url: "",
+    url: "mongodb+srv://nicoblanco208:dRnI9Oplvz5chlm0@cluster0.7n1uhyu.mongodb.net/?retryWrites=true&w=majority",
 }
 
 let guild = {
-    id: '',
+    id: '1105241952565346444',
 }
 
 module.exports = { bot, db, guild }
